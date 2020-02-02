efc58b9c478994f42f4433bc673505d17a11801f
SASBoom <balrajshah@live.com>
2020-02-02 18:11:55 -0500

    Modify login form

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -2,49 +2,47 @@
   <div>
     <h1>Login</h1>
     <div id="form">
-      <b-col>
-        <b-form @submit="submit">
-          <b-row>
-            <b-col>
-              <b-form-group
-                id="input-group-1"
-                label="Username"
-                label-for="username"
-                description="This will need to be unique"
+      <b-form @submit="submit">
+        <b-row>
+          <b-col>
+            <b-form-group
+              id="input-group-1"
+              label="Username"
+              label-for="username"
+              description=""
+            >
+              <b-form-input
+                id="username"
+                v-model="username"
+                type="text"
+                required
+                placeholder="Please enter a username"
               >
-                <b-form-input
-                  id="username"
-                  v-model="username"
-                  type="text"
-                  required
-                  placeholder="Enter username"
-                >
-                </b-form-input>
-              </b-form-group>
-            </b-col>
-          </b-row>
-          <b-row>
-            <b-col>
-              <b-form-group
-                id="input-group-2"
-                label="password"
-                label-for="password"
-                description=""
+              </b-form-input>
+            </b-form-group>
+          </b-col>
+        </b-row>
+        <b-row>
+          <b-col>
+            <b-form-group
+              id="input-group-2"
+              label="Password"
+              label-for="password"
+              description=""
+            >
+              <b-form-input
+                id="password"
+                v-model="password"
+                type="password"
+                required
+                placeholder="Please enter a password"
               >
-                <b-form-input
-                  id="password"
-                  v-model="password"
-                  type="password"
-                  required
-                  placeholder="Enter password"
-                >
-                </b-form-input>
-              </b-form-group>
-            </b-col>
-          </b-row>
-          <b-button type="submit" variant="primary">Submit</b-button>
-        </b-form>
-      </b-col>
+              </b-form-input>
+            </b-form-group>
+          </b-col>
+        </b-row>
+        <b-button type="submit" variant="dark">Submit</b-button>
+      </b-form>
     </div>
   </div>
 </template>
@@ -91,5 +89,7 @@ h1 {
 }
 #form {
   margin-top: 10vh;
+  margin-left: 20vw;
+  margin-right: 20vw;
 }
 </style>
