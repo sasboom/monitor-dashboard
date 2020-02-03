354d229cde77dd22f338050128cacb24c8492ec3
SASBoom <balrajshah@live.com>
2020-02-02 19:16:38 -0500

    Update navbar styling and add logout

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,12 +1,11 @@
 <template>
   <div>
-      <b-navbar type="dark" variant="dark" sticky>
-        <b-navbar-brand href="#/">Are You Here Yet?</b-navbar-brand>
-        <b-navbar-nav>
-        <b-nav-item href="#/login">Login</b-nav-item>
-        <b-nav-item href="#/Register">Register</b-nav-item>
-        <b-nav-item href="#/status">Update Status</b-nav-item>
-        <b-nav-item href="#/history">History</b-nav-item>
+      <b-navbar type="dark" variant="light">
+        <b-navbar-brand to="/"><h1>Are You Here Yet?</h1></b-navbar-brand>
+        <b-navbar-nav float-right>
+        <b-nav-item to="/status"><b-button variant="outline-dark">Update Status</b-button></b-nav-item>
+        <b-nav-item to="/history"><b-button variant="outline-dark">History <b-badge variant="success">New</b-badge></b-button></b-nav-item>
+        <b-nav-item @click="logout"><b-button variant="outline-danger"><b-icon icon="power"></b-icon> Logout</b-button></b-nav-item>
         </b-navbar-nav>
       </b-navbar>
     </div>
@@ -14,10 +13,20 @@
 
 <script>
 export default {
-  name: 'Navbar'
+  name: 'Navbar',
+  methods: {
+    logout: function () {
+      localStorage.clear();
+      this.$router.push('/login');
+    }
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+h1 {
+  font-family: 'BioRhyme', serif;
+  color: black
+}
 </style>
