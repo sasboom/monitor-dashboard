b372a042480365b6474f6cdc18aa5951b72c36c4
SASBoom <balrajshah@live.com>
2020-02-02 19:16:21 -0500

    Prevent users from relogging multiple times

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -1,50 +1,52 @@
 <template>
-  <div>
-    <h1>Login</h1>
-    <div id="form">
-      <b-form @submit="submit">
-        <b-row>
-          <b-col>
-            <b-form-group
-              id="input-group-1"
-              label="Username"
-              label-for="username"
-              description=""
-            >
-              <b-form-input
-                id="username"
-                v-model="username"
-                type="text"
-                required
-                placeholder="Please enter a username"
+  <transition name="fade" mode="out-in">
+    <div>
+      <h1>Login</h1>
+      <div id="form">
+        <b-form @submit="submit">
+          <b-row>
+            <b-col>
+              <b-form-group
+                id="input-group-1"
+                label="Username"
+                label-for="username"
+                description=""
               >
-              </b-form-input>
-            </b-form-group>
-          </b-col>
-        </b-row>
-        <b-row>
-          <b-col>
-            <b-form-group
-              id="input-group-2"
-              label="Password"
-              label-for="password"
-              description=""
-            >
-              <b-form-input
-                id="password"
-                v-model="password"
-                type="password"
-                required
-                placeholder="Please enter a password"
+                <b-form-input
+                  id="username"
+                  v-model="username"
+                  type="text"
+                  required
+                  placeholder="Please enter a username"
+                >
+                </b-form-input>
+              </b-form-group>
+            </b-col>
+          </b-row>
+          <b-row>
+            <b-col>
+              <b-form-group
+                id="input-group-2"
+                label="Password"
+                label-for="password"
+                description=""
               >
-              </b-form-input>
-            </b-form-group>
-          </b-col>
-        </b-row>
-        <b-button type="submit" variant="dark">Submit</b-button>
-      </b-form>
+                <b-form-input
+                  id="password"
+                  v-model="password"
+                  type="password"
+                  required
+                  placeholder="Please enter a password"
+                >
+                </b-form-input>
+              </b-form-group>
+            </b-col>
+          </b-row>
+          <b-button type="submit" variant="dark">Submit</b-button>
+        </b-form>
+      </div>
     </div>
-  </div>
+  </transition>
 </template>
 
 <script>
@@ -53,7 +55,8 @@ export default {
   data: function () {
     return {
         username: '',
-        password: ''
+        password: '',
+        authenticated: false
     }
   },
   methods: {
@@ -64,6 +67,7 @@ export default {
         .then(response => {
           localStorage.clear();
           localStorage.setItem('token', response.data.token);
+          this.authenticated = true;
           this.$router.push({ path: '/status' });
         })
         .catch(() => {
@@ -71,6 +75,15 @@ export default {
         })
       }
   },
+  mounted: function () {
+    const token = localStorage.getItem('token');
+    if (token) {
+      this.authenticated = true;
+      this.$router.push({path: '/status'});
+    } else {
+      this.authenticated = false;
+    }
+  }
 }
 </script>
 
